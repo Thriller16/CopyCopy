7a3326cc8b1259adba5bf9c0c7ee5a4bd5133c5f
Thriller16 <thrillerbrown106@gmail.com>
2018-05-03 01:16:08 +0000

    Just before useing fragments

--- app/src/main/java/com/lawrene/falcon/copycopy/MyFirebaseMessagingService.java
+++ app/src/main/java/com/lawrene/falcon/copycopy/MyFirebaseMessagingService.java
@@ -1,5 +1,6 @@
 package com.lawrene.falcon.copycopy;
 
+import android.app.Notification;
 import android.app.Service;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -18,101 +19,44 @@ import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.Job;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-/**
- * Created by lawrene on 4/28/18.
- */
+
+
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
-    
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        String notification_title = remoteMessage.getNotification().getTitle();
+        String notification_message = remoteMessage.getNotification().getBody();
+        String click_action = remoteMessage.getNotification().getClickAction();
 
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                handleNow();
-            }
-
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-    }
-    // [END receive_message]
-
-    /**
-     * Schedule a job using FirebaseJobDispatcher.
-     */
-    private void scheduleJob() {
-        // [START dispatch_job]
-        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
-        Job myJob = dispatcher.newJobBuilder()
-                .setService(MyJobService.class)
-                .setTag("my-job-tag")
-                .build();
-        dispatcher.schedule(myJob);
-        // [END dispatch_job]
-    }
-
-    /**
-     * Handle time allotted to BroadcastReceivers.
-     */
-    private void handleNow() {
-        Log.d(TAG, "Short lived task is done.");
-    }
-
-    /**
-     * Create and show a simple notification containing the received FCM message.
-     *
-     * @param messageBody FCM message body received.
-     */
-    private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
-        String channelId = getString(R.string.default_notification_channel_id);
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.drawable.ic_notifications_black_24dp)
-                        .setContentTitle("FCM Message")
-                        .setContentText(messageBody)
-                        .setAutoCancel(true)
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher).setAutoCancel(true)
+                .setContentTitle(notification_title)
                         .setSound(defaultSoundUri)
-                        .setContentIntent(pendingIntent);
+                .setContentText(notification_message);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent resultIntent = new Intent(click_action);
 
-        // Since android Oreo notification channel is needed.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(channelId,
-                    "Channel human readable title",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            notificationManager.createNotificationChannel(channel);
-        }
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
+        int mNotificationId = (int)System.currentTimeMillis();
+        NotificationManager mNotifyMgr =
+                (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
     }
-
-//    onMessa
 }
--- notificationFunction/functions/index.js
+++ notificationFunction/functions/index.js
@@ -5,23 +5,37 @@ admin.initializeApp(functions.config().firebase);
 
 exports.sendNotification = functions.database.ref('/Notifications/{user_id}/{notification_id}').onWrite((data, context) => {
   					const user_id = context.params.user_id;
-					const notification = context.params.notification_id;
+					const notification_id = context.params.notification_id;
 
   console.log('User id is : ', user_id);
-				
-					const deviceToken = admin.database().ref(`/Users/${user_id}/device_token`).once('value');
+  console.log('Notification id is : ', notification_id);
+  
+  
+  const courseName = admin.database().ref(`/Notifications/${user_id}/${notification_id}`).once('value');
 
-					return deviceToken.then(result =>{
-						const token_id = result.val();
-						const payload = {
-							notification: {
-								title: "New Assignment",
-								body: "You have a new assignment upload",
-							}
-						};
+  return courseName.then(courseNameResult =>{
+
+	  const course_title = courseNameResult.val().title;
+	  const course_message = courseNameResult.val().message;
+	  
+	// return console.log("The course title is" , course_title, "And the message is " , course_message);
 	
-						return admin.messaging().sendToDevice(token_id, payload).catch((err) =>{
-							console.log("err is ", err);
-						})
-					});
+	const deviceToken = admin.database().ref(`/Users/${user_id}/device_token`).once('value');
+
+	return deviceToken.then(result =>{
+		const token_id = result.val();
+		const payload = {
+			notification: {
+				title: course_title,
+				body: course_message,
+				icon: "default",
+				click_action: "com.lawrene.falcon.copycopy_TARGET_NOTIFICATION"
+			}
+		};
+
+		return admin.messaging().sendToDevice(token_id, payload).catch((err) =>{
+			console.log("err is ", err);
+		})
+	});
+  });
 });
